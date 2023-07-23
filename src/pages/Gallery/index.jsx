b0eaba9a93b990c699bd7beec42c41708f7c9d4d
jekisaputra7 <saputrajeki126@gmail.com>
2023-07-24 04:56:27 +0700

    bug galery

--- src/pages/Gallery/index.jsx
+++ src/pages/Gallery/index.jsx
@@ -14,47 +14,47 @@ import imgGallery12 from "../../assets/imagesIptij/img12.jpg";
 
 const Gallery = () => {
   return (
-    <div className="md:grid md:grid-cols-6 flex flex-col  gap-2 w-[95%] m-auto mb-4  ">
-      <div className="col-span-6">
+    <div className="md:grid  grid-cols-12 grid  gap-2 w-[95%] m-auto mb-4  ">
+      <div className="col-span-12">
         <Banner />
       </div>
-      <div className="col-span-6 mt-8  animate__animated  animate__fadeInDown">
+      <div className="col-span-12 mt-8  animate__animated  animate__fadeInDown">
         <p className="text-center text-3xl font-semibold">Gallery</p>
         <hr className="mb-4 border-orange-400 col-span-4 w-[50%] m-auto" />
       </div>
       {/* grid image */}
-      <div className="box md:col-span-3  ">
-        <img className="md:imgStyle  " src={imgGallery1} alt="" />
+      <div className="box md:col-span-3 col-span-6    ">
+        <img className="md:imgStyle h-full " src={imgGallery1} alt="" />
       </div>
-      <div className=" box md:row-span-2 md:col-span-2 ">
+      <div className=" box md:row-span-2 md:col-span-2 col-span-6   ">
         <img className="imgStyle" src={imgGallery4} alt="" />
       </div>
-      <div className="box  ">
+      <div className="box col-span-4 ">
         <img className="imgStyle" src={imgGallery7} alt="" />
       </div>
-      <div className=" box md:col-span-2 ">
+      <div className=" box md:col-span-2 col-span-8 ">
         <img className="imgStyle" src={imgGallery2} alt="" />
       </div>
-      <div className="box ">
+      <div className="box col-span-8 ">
         <img className="imgStyle" src={imgGallery10} alt="" />
       </div>
-      <div className="box  ">
+      <div className="box  col-span-4 ">
         <img className="imgStyle" src={imgGallery12} alt="" />
       </div>
-      <div className="box col-span-3 ">
+      <div className="box md:col-span-3  col-span-6  row-span-6 ">
         <img className="imgStyle" src={imgGallery3} alt="" />
       </div>
-      <div className="box col-span-3 ">
+      <div className="box md:col-span-2 col-span-6 row-span-6 ">
+        <img className="imgStyle " src={imgGallery6} alt="" />
+      </div>
+      <div className="box col-span-12 ">
         <img className="imgStyle" src={imgGallery9} alt="" />
       </div>
-      <div className="box md:col-span-2">
+      <div className="box md:col-span-2 col-span-6  ">
         <img className="imgStyle" src={imgGallery5} alt="" />
       </div>
-      <div className="box md:col-span-2">
+      <div className="box md:col-span-2 col-span-6">
         <img className="imgStyle" src={imgGallery11} alt="" />
-      </div>
-      <div className="box md:col-span-2">
-        <img className="imgStyle" src={imgGallery6} alt="" />
       </div>
     </div>
   );
