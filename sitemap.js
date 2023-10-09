773a6a022ce906a363fe092acdf0ac2027efd937
Jeki Saputra <saputrajeki90@gmail.com>
2023-10-09 23:15:13 +0700

    revised

--- sitemap.js
+++ sitemap.js
@@ -1,18 +1,25 @@
-import { createSitemap as _createSitemap } from "sitemap";
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { Readable } from 'stream';
 
-const createSitemap = () => {
-  const sitemap = _createSitemap({
-    hostname: "https://www.iptijtokyo.com",
-    urls: [
-      { url: "/", changefreq: "daily", priority: 1.0 },
-      { url: "/", changefreq: "weekly", priority: 0.8 },
-      { url: "/jadwalsholat", changefreq: "weekly", priority: 0.8 },
-      { url: "/gallery", changefreq: "weekly", priority: 0.8 },
-      { url: "/kajian", changefreq: "weekly", priority: 0.8 },
-      // Tambahkan URL lainnya di sini
-    ],
+const createSitemap = async () => {
+  const stream = new SitemapStream({ hostname: 'https://www.iptijtokyo.com' });
+
+  const urls = [
+    { url: '/', changefreq: 'daily', priority: 1.0 },
+    { url: '/page1', changefreq: 'weekly', priority: 0.8 },
+    { url: '/jadwalsholat', changefreq: 'weekly', priority: 0.8 },
+    { url: '/gallery', changefreq: 'weekly', priority: 0.8 },
+    { url: '/kajian', changefreq: 'weekly', priority: 0.8 },
+    // Tambahkan URL lainnya di sini
+  ];
+
+  urls.forEach((url) => {
+    stream.write(url);
   });
 
+  stream.end();
+
+  const sitemap = await streamToPromise(Readable.from([stream]));
   return sitemap.toString();
 };
 
