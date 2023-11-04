6c596c50003ea34cb4d90f9922e1e0455ef2a603
Jeki Saputra <saputrajeki90@gmail.com>
2023-11-05 02:07:40 +0700

    revisi

--- src/Components/Atoms/Search/index.jsx
+++ src/Components/Atoms/Search/index.jsx
@@ -1,12 +1,11 @@
-/* eslint-disable react/prop-types */
 // eslint-disable-next-line react/prop-types
 
-const SearchBar = ({ placeholder, onChange, onClick, icon }) => {
+const SearchBar = ({ title, placeholder, onChange, onClick, icon }) => {
   return (
-    <div className="flex  w-[100%]  justify-center">
+    <div className="flex w-[100%] justify-center ">
       <input
         onChange={onChange}
-        className=" py-[0.3em] px-[3em] md:px-[5em] relative text-start  rounded-full   border border-gray-300 text-xl "
+        className=" py-[0.3em] px-[3em] md:px-[5em] text-black relative text-start  rounded-full w-[100%] flex justify-center items-center border border-gray-300 text-xl "
         type="text"
         placeholder={placeholder}
       />
