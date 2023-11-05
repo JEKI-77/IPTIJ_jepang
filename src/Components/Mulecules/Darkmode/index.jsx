f1fdb503b6940921583280c5e3de2b261d823bcf
Jeki Saputra <saputrajeki90@gmail.com>
2023-11-06 06:49:05 +0700

    whatsapp icon

--- src/Components/Mulecules/Darkmode/index.jsx
+++ src/Components/Mulecules/Darkmode/index.jsx
@@ -4,7 +4,7 @@ const DarkMode = ({ isChecked, handleToggle }) => {
   return (
     <div>
       {/* darkmode */}
-      <label className="flex flex-1 items-center space-x-2 cursor-pointer mr-4 mt-1 md:mt-0  ">
+      <label className="flex flex-1 items-center space-x-2 cursor-pointer mr-4 mt-2 md:mt-0  ">
         <div className="relative">
           <input
             type="checkbox"
@@ -13,13 +13,13 @@ const DarkMode = ({ isChecked, handleToggle }) => {
             onChange={handleToggle}
           />
           <div
-            className={`md:w-10 md:h-5 w-8 h-4 rounded-full shadow-inner ${
+            className={`md:w-10 md:h-5 w-7 h-3 rounded-full shadow-inner ${
               isChecked ? " bg-black " : "bg-gray-400"
             }`}
           >
             <div
-              className={`md:w-5 md:h-5 w-4 h-4 bg-white rounded-full shadow-md transform transition-transform ${
-                isChecked ? "translate-x-6" : "translate-x-0 "
+              className={`md:w-5 md:h-5 w-3 h-3 bg-white rounded-full shadow-md transform transition-transform ${
+                isChecked ? "md:translate-x-6 translate-x-4" : "translate-x-0 "
               }`}
             />
           </div>
